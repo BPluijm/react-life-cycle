1242bd216b849e05f61f155af1a33060029eab01
BPluijm <75165735+BPluijm@users.noreply.github.com>
2021-03-29 16:44:58 +0200

    lifecycle oefening

--- src/components/NameItem.js
+++ src/components/NameItem.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-function NameItem({ name }) {
+function NameItem({ name, activeName, setActiveName }) {
+
+    useEffect(() => {
+        console.log(`${name} is in de DOM geplaatst`);
+    }, []);
+
+    useEffect (() => {
+        if (name === activeName) {
+            console.log(`${name} wil graag een biertje bestellen`)
+        }
+    }, [name, activeName]);
+
+
   return (
     <li>
       <h3>{name}</h3>
-      <button type="button">
-        Geef een rondje!
+      <button
+          type="button"
+          onClick={() => setActiveName(name)}>
+          Geef een rondje!
       </button>
     </li>
   );
